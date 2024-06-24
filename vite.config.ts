67779628329c110ed23f06304bf45a9d3812f288
muhammadumair12345 <hafizumairtariq1234@gmail.com>
2024-06-25 00:47:40 +0500

    Updated Package

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: 'src/components/index.ts', // Adjust to your entry file
+      name: 'ReactVirtualDynamics',
+      fileName: (format) => `react-virtual-dynamics.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 });
