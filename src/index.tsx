33d614325e25727a7e764d2167b63591ba890ded
muhammadumair12345 <hafizumairtariq1234@gmail.com>
2024-06-26 22:23:50 +0500

    updated package

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import VirtualizedList from './components/VirtualizedList';
 
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
-);
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+export default VirtualizedList;
